5146aa02d82cb40d3dcbab13b6477fa2992b0218
tribuzio <cindy.tribuzio@noaa.gov>
2022-08-16 14:16:13 -0800

    updated obs special proj data

--- Obj1_obs_size_method/Code/Clean_data_code.R
+++ Obj1_obs_size_method/Code/Clean_data_code.R
@@ -1,33 +1,57 @@
 # Title: EM Large Sharks: Obj 1 - cleaning data ----
-# The below documents the steps inputting OBS special project data
-# and merging with NORPAC haul data
+# The below documents the steps for downloading NORPAC data directly from AFSC
+# inputting OBS special project data, merging with NORPAC haul data
+# and making non-confidential location info
 # Updated: Jul 29 2022
 # Recent Author: Cindy Tribuzio
 
 # TO DO ----
-#1) automate NORPAC haul data query, pulling data via AKFIN requires 
-#   separate queries for each year due to large file sizes
-#2) automate pulling data from google sheet
-#3) figure out why duplicates are added
+#1) automate pulling data from google sheet
+#2) figure out why duplicates are added
 
 # Setup ----
 libs <- c("tidyverse", "janitor", "Hmisc", "lubridate","sp","sf", 
-          "rnaturalearth", "rgdal", "rnaturalearthdata","spatialEco")
+          "rnaturalearth", "rgdal", "rnaturalearthdata","spatialEco", "RODBC")
 if(length(libs[which(libs %in% rownames(installed.packages()) == FALSE )]) > 0) {
   install.packages(libs[which(libs %in% rownames(installed.packages()) == FALSE)])}
 lapply(libs, library, character.only = TRUE)
 '%nin%'<-Negate('%in%') #this is a handy function
 
-datadir<-paste(getwd(),"/Obj1_obs_size_method/Data/",sep="")
+datadir<-paste(getwd(),"/Obj1_obs_size_method/Data/Raw_Data/",sep="")
 outdir<-paste(getwd(),"/Obj1_obs_size_method/Data/Cleaned_Data/",sep="")
+shapedir <- paste(getwd(),"/Obj1_obs_size_method/Data/Shapefile/",sep="")
 
-# Bring in data ----
+# Get NORPAC Data ----
 # NORPAC haul data, queried from AKFIN by year
-norpac <- read_csv(paste(datadir, "Raw_Data/confidential_norpac_catch_report.csv", sep = "")) %>% 
+# skip this section if data have already been queried
+
+dbname <- "afsc"
+db <- read_csv('database.csv')
+database_afsc=db %>% filter(database == dbname) %>% select(database) #need to add filter for AKFIN user/pass only
+username_afsc=db %>% filter(database == dbname) %>% select(username)
+password_afsc=db %>% filter(database == dbname) %>% select(password)
+
+assign(paste0("channel_", dbname), odbcConnect(dbname, uid = username_afsc, pwd = password_afsc, believeNRows=FALSE))
+
+
+NORPAC_haul <- sqlQuery(channel_afsc, query = ("
+                select    *
+                from      obsint.debriefed_haul
+                where     year >= 2018")) %>% 
   clean_names() %>% 
-  mutate(haul_date = dmy(haul_date))
-# OBS special project
-obsSP <- read_csv(paste(datadir, "Raw_Data/confidential_NPGOP_PSS_length.csv", sep = "")) %>% 
+  mutate(retrieval_date = dmy(haul_date))
+
+write_csv(NORPAC_haul, paste0(datadir, "confidential_NORPAChaul_18_22.csv"))
+rm(NORPAC_haul)
+
+# read in queried data
+norpac <- read_csv(paste0(datadir, "confidential_NORPAChaul_18_22.csv")) %>% 
+  select(cruise, permit, year, haul, haul_date, nmfs_area, latdd_start, londd_start) %>% 
+  mutate(haul_date = floor_date(haul_date, "day"))
+
+
+# OBS special project Data ----
+obsSP <- read_csv(paste(datadir, "confidential_NPGOP_PSS_length.csv", sep = "")) %>% 
   mutate(Haul = as.numeric(gsub("/", "", Haul))) %>% 
   clean_names() %>% 
   rename("gear_desc" = "gear",
@@ -56,22 +80,21 @@ round2match <-nomatch %>%
   rename("cruise" = "cruise.y") %>% 
   select(!cruise.x)# add this to obsSP_haul
 
-#stop here because getting more complete NORPAC data may solve some of this
 nomatch2 <- nomatch %>% 
   left_join(norpac, by = c("year","permit", "haul", "haul_date"))%>% 
   filter(is.na(nmfs_area)) 
 
-# error check individuals
+# error check individuals as needed
+write_csv(nomatch2, paste0(datadir, "confidential_non_matched_data.csv"))
+#write_csv(norpac, paste0(datadir, "confidential_cleaned_norpac.csv"))
 
 # combine merged objects
 obsSP_conf <- round1match %>% 
   bind_rows(round2match) %>% 
-  mutate(sampID = paste(species_code, "_", year, "_", cruise, "_", permit, "_", haul, "_", row_number(), sep=""))
+  mutate(sampID = paste(62, "_", year, "_", cruise, "_", permit, "_", haul, "_", row_number(), sep=""))
 
-
-#add in confidentiality steps
 # Spatial Joins for making non-confidential ----
-AKncf_grid <- readOGR(dsn = paste(datadir,"Shapefile/20kmhexagon_clip",sep=""),
+AKncf_grid <- readOGR(dsn = paste(shapedir,"20kmhexagon_clip",sep=""),
                       layer = "20kmhexagon_clip")
 # view grid
 plot(AKncf_grid)
@@ -90,8 +113,8 @@ plot(AKncf_grid)
   
 # Create smaller set dataframe without extra cols
 set_sm <- obsSP_conf %>% 
-  select(lon = lon_dd_end, 
-         lat = lat_dd_end, 
+  select(lon = londd_start, 
+         lat = latdd_start, 
          sampID) %>% 
   data.frame
 
